1a246c9b0ac874d821957afb2bdea7bef98f0aee
ejr004 <acessoej@icloud.com>
2018-09-27 11:03:28 +0200

    #6 initial changes to the run script

--- yaim/run_lwwn.sh
+++ yaim/run_lwwn.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-sudo systemstl stop firewalld
+ipcontainer=$1
+hostnamecontainer=$2
+ipcream=$3
+hostnamecream=$4
+netname=$5
 
+sudo systemctl stop firewalld
+sudo docker build --rm  -t lwwn-umd4 .
 sudo docker run -d \
-	-it \
-	--name lwwn \
-	--net host \
-	--mount type=bind,source="$(pwd)"/wn-config,target=/wn-config \
-	maany/lightweight-site-wn \
-	/bin/bash
+        -itd \
+        --privileged \
+        --name $hostnamecontainer \
+        --net $netname \
+        --ip $ipcontainer \
+        --hostname $hostnamecontainer \
+        --add-host $hostnamecream:$ipcream \
+        --mount type=bind,source="$(pwd)"/wn-config,target=/wn-config \
+        maany/lwwn-umd4 \
+        /bin/bash
+
+#sudo docker exec -it lwwn-umd4 /wn-config/init.sh
