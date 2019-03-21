30b7799443b7fbf90bb3ea821274ad7cd2987a71
maany <imptodefeat@gmail.com>
2019-03-21 19:23:40 +0100

    update meta-info and init.sh

--- yaim/config/init.sh
+++ yaim/config/init.sh
@@ -2,15 +2,15 @@
 
 # move configuration files to the correct place
 echo "Copying configuraiton files..."
-cp /config/wn-info.def /root/
-cp /config/users.conf /root/
-cp /config/groups.conf /root/
-cp /config/wn-list.conf /root/
+cp /etc/simple_grid/config/wn-info.def /root/
+cp /etc/simple_grid/config/users.conf /root/
+cp /etc/simple_grid/config/groups.conf /root/
+cp /etc/simple_grid/config/wn-list.conf /root/
 
 # run YAIM
 echo "Starting YAIM..."
 /opt/glite/yaim/bin/yaim -c \
-	-s /config/wn-info.def \
+	-s /etc/simple_grid/config/wn-info.def \
 	-n WN \
 	-n TORQUE_client
 
